5ebb827f7bbc63afdf2c382fa2c4fcf1261947b8
MUHAMMET KOMUR <muhammet075@icloud.com>
2023-05-26 15:08:50 +0200

    working on header

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,28 @@ import styles from "../styles/header.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "../assets/img/header-logo.svg";
+import hamburgerIco from "../assets/icons/hamburger.svg";
+import closeIco from "../assets/icons/white-close.svg";
 
 function Header() {
 
   useEffect(() => {
   }, []);
+
+  function openHamburger(){
+    document.querySelector(".hamburger").classList.add("blockhamburger")
+    document.querySelector(".hamburgermenu").classList.remove("closehamburger");
+    document.querySelector(".hamburgermenu").classList.add("openhamburger")
+  }
+
+  function closeHamburger(){
+      document.querySelector(".hamburgermenu").classList.remove("openhamburger");
+      document.querySelector(".hamburgermenu").classList.add("closehamburger")
+      setTimeout(() => {
+          document.querySelector(".hamburger").classList.remove("blockhamburger");
+      }, 300);
+
+  }
 
   return (
     <div className={styles.header}>
@@ -25,20 +42,25 @@ function Header() {
         </section>
 
         <section></section>
+
+        <button onClick={openHamburger}><Image src={hamburgerIco} alt="Hamburger menu Icoon"/></button>
       </div>
 
-      <div>
-        <nav>
+      <div className="hamburger">
+        <nav className="hamburgermenu">
           <section>
             <ul>
-              <li><Link href="/">Home</Link></li>
-              <li><Link href="/">Prijzen</Link></li>
-              <li><Link href="/">Over Ons</Link></li>
-              <li><Link href="/">Rij Gebieden</Link></li>
-              <li><Link href="/">Afspraak</Link></li>
+              <li><Link href="/" onClick={closeHamburger}>Home</Link></li>
+              <li><Link href="/" onClick={closeHamburger}>Prijzen</Link></li>
+              <li><Link href="/" onClick={closeHamburger}>Over Ons</Link></li>
+              <li><Link href="/" onClick={closeHamburger}>Rij Gebieden</Link></li>
+              <li><Link href="/" onClick={closeHamburger}>Afspraak</Link></li>
             </ul>
           </section>
         </nav>
+        <div onClick={closeHamburger}>
+          <Image src={closeIco} alt="Sluit Icoon"/>
+        </div>
       </div>
 
       <div></div>
